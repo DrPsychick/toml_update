81062732e0ae9416a4d95931326b7aa2e6f603a2
DrPsychick <drpsychick@drsick.net>
2021-05-13 13:25:52 +0200

    fix: format + refactor

--- toml_update.go
+++ toml_update.go
@@ -2,67 +2,66 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"github.com/pelletier/go-toml"
 	"os"
-	"strings"
 	"strconv"
-    "github.com/pelletier/go-toml"
+	"strings"
 )
 
 func updateConfigFromEnv(conf []byte, pfx string) ([]byte, error) {
 	config, err := toml.Load(string(conf))
 	if err != nil {
-	    return nil, err
+		return nil, err
 	}
 
-	var envs = make(map[string]string)
+	envs := make(map[string]string)
 	for _, e := range os.Environ() {
 		if !strings.HasPrefix(e, pfx) {
 			continue
 		}
 		// PFX_VARIABLE=section.sub.name="value"
-		parts := strings.Split(e, "=")
+		parts := strings.SplitN(e, "=", 2)
 
 		// trim quotes from the value
-		envs[parts[1]] = strings.Trim(strings.Join(parts[2:], "="), "\"")
+		envs[parts[1]] = strings.Trim(parts[2], "\"")
 	}
 	if len(envs) == 0 {
 		return nil, fmt.Errorf("No ENVs with prefix %s", pfx)
 	}
 
 	for k, v := range envs {
-	    var withComment = false
-	    if strings.HasPrefix(v, "#") {
-	        withComment = true
-	        v = strings.TrimLeft(v, "#")
-	    }
+		withComment := false
+		if strings.HasPrefix(v, "#") {
+			withComment = true
+			v = strings.TrimLeft(v, "#")
+		}
 
-	    // set value with correct type
-	    var val interface{}
-	    if i, err := strconv.ParseInt(v, 10, 64); err == nil {
-	        val = i
-	    } else if f, err := strconv.ParseFloat(v, 64); err == nil {
-	        val = f
-	    } else if b, err := strconv.ParseBool(v); err == nil {
-	        val = b
-	    } else if strings.HasPrefix(v, "[") {
-	        // ["foo", "bar"] - we have to remove the quotes or they will be escaped and included
-	        list := strings.Split(strings.Trim(v, "[]"), ",")
-	        for i, v := range list {
-	            // trim quotes and spaces from each element
-	            list[i] = strings.Trim(v, "\" ")
-	        }
-	        val = list
-	    } else {
-	        // default is string with quotes
-	        val = v
-        }
+		// set value with correct type
+		var val interface{}
+		if i, err := strconv.ParseInt(v, 10, 64); err == nil {
+			val = i
+		} else if f, err := strconv.ParseFloat(v, 64); err == nil {
+			val = f
+		} else if b, err := strconv.ParseBool(v); err == nil {
+			val = b
+		} else if strings.HasPrefix(v, "[") {
+			// ["foo", "bar"] - we have to remove the quotes or they will be escaped and included
+			list := strings.Split(strings.Trim(v, "[]"), ",")
+			for i, v := range list {
+				// trim quotes and spaces from each element
+				list[i] = strings.Trim(v, "\" ")
+			}
+			val = list
+		} else {
+			// default is string with quotes
+			val = v
+		}
 
-        if withComment {
-            config.SetWithComment(k, "", true, val)
-        } else {
-            config.Set(k, val)
-        }
+		if withComment {
+			config.SetWithComment(k, "", true, val)
+		} else {
+			config.Set(k, val)
+		}
 	}
 
 	out, err := toml.Marshal(config)
@@ -70,25 +69,25 @@ func updateConfigFromEnv(conf []byte, pfx string) ([]byte, error) {
 }
 
 func main() {
-	var conf_file = os.Getenv("CONF_UPDATE")
-	var prefix = os.Getenv("CONF_PREFIX")
+	conf_file := os.Getenv("CONF_UPDATE")
+	prefix := os.Getenv("CONF_PREFIX")
 	if conf_file == "" || prefix == "" {
-        fmt.Println("# No CONF_UPDATE or CONF_PREFIX defined - exiting.")
+		fmt.Println("# No CONF_UPDATE or CONF_PREFIX defined - exiting.")
 		os.Exit(0)
 	}
 
-    var buf []byte
-    var err error
-	if buf, err = ioutil.ReadFile(conf_file); err != nil {
-	    fmt.Printf("Failed to read config file: %s\n", conf_file)
-	    os.Exit(1)
+	var buf []byte
+	var err error
+	if buf, err = os.ReadFile(conf_file); err != nil {
+		fmt.Printf("Failed to read config file: %s\n", conf_file)
+		os.Exit(1)
 	}
 	if buf, err = updateConfigFromEnv(buf, prefix); err != nil {
-	    fmt.Printf("Failed to update config from ENV: %s\n", err)
-	    os.Exit(1)
+		fmt.Printf("Failed to update config from ENV: %s\n", err)
+		os.Exit(1)
 	}
-	if err = ioutil.WriteFile(conf_file, buf, 0644); err != nil {
-	    fmt.Printf("Failed to write back config to file '%s': %s\n", conf_file, err)
-	    os.Exit(1)
+	if err = os.WriteFile(conf_file, buf, 0644); err != nil {
+		fmt.Printf("Failed to write back config to file '%s': %s\n", conf_file, err)
+		os.Exit(1)
 	}
 }
