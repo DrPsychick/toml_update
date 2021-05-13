f7221d333547cecd6ed9c87681e72273a4091e74
DrPsychick <drpsychick@drsick.net>
2021-05-13 20:54:48 +0200

    feat: improve tests

--- toml_update_test.go
+++ toml_update_test.go
@@ -6,23 +6,27 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNoEnv(t *testing.T) {
 	os.Clearenv()
-	envs := getEnv("PFX")
-	assert.Empty(t, envs, "Environment variables must be empty")
+
+	got := getEnv("PFX")
+
+	assert.Empty(t, got, "Environment variables must be empty")
 }
 
 func TestEmptyEnv(t *testing.T) {
 	os.Clearenv()
 	os.Setenv("PFX_Test", "")
 	os.Setenv("PFX_Test2", "foo")
-	envs := getEnv("PFX")
-	assert.Len(t, envs, 0, "Environment variables must be skipped")
 
-	envs = getEnv("FOO")
-	assert.Len(t, envs, 0, "Environment variables must be skipped")
+	got := getEnv("PFX")
+	assert.Len(t, got, 0, "Environment variables must be skipped")
+
+	got = getEnv("FOO")
+	assert.Len(t, got, 0, "Environment variables must be skipped")
 }
 
 func TestSimpleEnv(t *testing.T) {
@@ -30,60 +34,68 @@ func TestSimpleEnv(t *testing.T) {
 	os.Setenv("PFX_Test", "foo.bar=bar")
 	os.Setenv("PFX_Test2", "bar=")
 
-	envs := getEnv("PFX")
-	assert.Len(t, envs, 2, "Environment variables must be skipped")
-	assert.Equal(t, "bar", envs["foo.bar"])
-	assert.Equal(t, "", envs["bar"])
+	got := getEnv("PFX")
+
+	require.Len(t, got, 2, "Environment variables must be skipped")
+	assert.Equal(t, "bar", got["foo.bar"])
+	assert.Equal(t, "", got["bar"])
 }
 
 func TestListEnv(t *testing.T) {
 	os.Clearenv()
 	os.Setenv("PFX_TEST", "foo=[\"bar\", \"foo\"]")
-	envs := getEnv("PFX")
 
-	assert.Len(t, envs, 1, "Environment variables must be skipped")
-	assert.Equal(t, "[\"bar\", \"foo\"]", envs["foo"])
+	got := getEnv("PFX")
+
+	require.Len(t, got, 1, "Environment variables must be skipped")
+	assert.Equal(t, "[\"bar\", \"foo\"]", got["foo"])
 }
 
 func TestIntEnv(t *testing.T) {
 	os.Clearenv()
 	os.Setenv("PFX_Test", "foo=\"1\"")
-	envs := getEnv("PFX")
 
-	assert.Len(t, envs, 1)
-	assert.Equal(t, "1", envs["foo"])
+	got := getEnv("PFX")
+
+	require.Len(t, got, 1)
+	assert.Equal(t, "1", got["foo"])
 }
 
 func TestParseInt(t *testing.T) {
-	val := parseValue("1")
-	assert.Equal(t, int64(1), val)
+	got := parseValue("1")
+	assert.Equal(t, int64(1), got)
 
-	val = parseValue("2.5")
-	assert.NotEqual(t, int64(2), val)
+	got = parseValue("2.5")
+	assert.NotEqual(t, int64(2), got)
 }
 
 func TestParseFloat(t *testing.T) {
-	val := parseValue("1.5")
-	assert.Equal(t, float64(1.5), val)
+	got := parseValue("1.5")
+	assert.Equal(t, float64(1.5), got)
+}
+
+func TestPrseBool(t *testing.T) {
+	got := parseValue("true")
+	assert.Equal(t, true, got)
 }
 
 func TestParseList(t *testing.T) {
-	val := parseValue("[\"foo\"]")
-	assert.Len(t, val, 1)
-	assert.Equal(t, []string{"foo"}, val)
+	got := parseValue("[\"foo\"]")
+	require.Len(t, got, 1)
+	assert.Equal(t, []string{"foo"}, got)
 
-	val = parseValue("[\"bar\", \"foo\", \"\"]")
-	assert.Len(t, val, 3)
-	assert.Equal(t, []string{"bar", "foo", ""}, val)
+	got = parseValue("[\"bar\", \"foo\", \"\"]")
+	require.Len(t, got, 3)
+	assert.Equal(t, []string{"bar", "foo", ""}, got)
 
-	val = parseValue("[]")
-	assert.IsType(t, []string{}, val)
-	assert.Len(t, val, 0)
+	got = parseValue("[]")
+	require.IsType(t, []string{}, got)
+	assert.Len(t, got, 0)
 }
 
 func TestParseString(t *testing.T) {
-	val := parseValue("foo bar")
-	assert.Equal(t, "foo bar", val)
+	got := parseValue("foo bar")
+	assert.Equal(t, "foo bar", got)
 }
 
 func TestConfNoEnv(t *testing.T) {
@@ -99,23 +111,24 @@ func TestUpdateConf(t *testing.T) {
 	os.Setenv("PFX_Test", "section.key=value")
 	config := []byte("\n[section]\n  key = \"value\"\n")
 
-	conf, err := updateConfigFromEnv(config, "PFX")
+	got, err := updateConfigFromEnv(config, "PFX")
 
 	assert.Nil(t, err)
-	assert.Equal(t, string(config), string(conf))
+	assert.Equal(t, string(config), string(got))
 }
 
 func TestComplexConfig(t *testing.T) {
 	os.Clearenv()
-	os.Setenv("PFX_Test0", "section.comment=#value")
+	os.Setenv("PFX_Test0", "section.comment=\"#value\"")
 	os.Setenv("PFX_Test1", "section.integer=1")
 	os.Setenv("PFX_Test2", "section.list=[\"foo\",\"bar\"]")
-	os.Setenv("PFX_Test3", "section.float=1.337")
+	os.Setenv("PFX_Test3", "section.float=\"1.337\"")
+	os.Setenv("PFX_Test3", "section.bool=\"false\"")
 
-	config := []byte("\n[section]\n  # comment = \"value\"\n  float = 1.337\n  integer = 1\n  list = [\"foo\", \"bar\"]\n")
+	config := []byte("\n[section]\n  bool = false\n  # comment = \"value\"\n  float = 1.337\n  integer = 1\n  list = [\"foo\", \"bar\"]\n")
 
-	conf, err := updateConfigFromEnv(config, "PFX")
+	got, err := updateConfigFromEnv(config, "PFX")
 
 	assert.Nil(t, err)
-	assert.Equal(t, string(config), string(conf))
+	assert.Equal(t, string(config), string(got))
 }
